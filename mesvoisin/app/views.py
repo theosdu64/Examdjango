9062402f1bbfad5ece3a195cd8f2c2ba241baeb0
theo <matchoc64600@gmail.com>
2026-03-09 15:29:26 +0100

    ajout de la logique dans la view pour recuperer les dernier service accepte

--- mesvoisin/app/views.py
+++ mesvoisin/app/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Service
+
 def home(request):
-    return render(request, 'home.html')
+    last_services_accepted = Service.objects.filter(volunteer_id__isnull=False)[:5]
+    context = {"last_services": last_services_accepted}
+    print(context)
+    return render(request, 'home.html', context)
