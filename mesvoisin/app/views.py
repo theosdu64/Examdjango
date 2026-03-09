454a1f2b55b9d629695e4771996de2c8d79ac0f3
theo <matchoc64600@gmail.com>
2026-03-09 15:46:40 +0100

    appelle de la liste de tt les skills disponible

--- mesvoisin/app/views.py
+++ mesvoisin/app/views.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from .models import Service
+from .models import Service,Skill
 
 def home(request):
     last_services_accepted = Service.objects.filter(volunteer_id__isnull=False)[:5]
-    context = {"last_services": last_services_accepted}
+    all_skill = Skill.objects.all()
+    context = {"last_services": last_services_accepted, "all_skill": all_skill}
     print(context)
     return render(request, 'home.html', context)
