372bdd8bccf61263afa5bf0a3addcfad3cd56d62
theo <matchoc64600@gmail.com>
2026-03-11 09:42:56 +0100

    ajou du chemin dans la navigation vers la page my_service

--- mesvoisin/app/urls.py
+++ mesvoisin/app/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("services/", views.services, name="services"),
     path("create-service/", views.create_service, name="create_service"),
-    path("postulez/<int:service_id>/", views.postulez, name="postulez"),
+    path("my-services/", views.my_services, name="my_services"),
 ]
