f8b6381e8c190a4b0920983c1edd27d8b2d4fc57
theo <matchoc64600@gmail.com>
2026-03-12 09:04:53 +0100

    ajout des fonction des delete et addskill dans lurl

--- mesvoisin/app/urls.py
+++ mesvoisin/app/urls.py
@@ -7,4 +7,9 @@ urlpatterns = [
     path("services/", views.services, name="services"),
     path("create-service/", views.create_service, name="create_service"),
     path("my-services/", views.my_services, name="my_services"),
+    path("exchange/", views.exchange, name="exchange"),
+    # Button OnClick
+    path("postulez/<int:service_id>/", views.postulez, name="postulez"),
+    path("delete-skill/<int:skill_id>/", views.deleteUserSkill, name="delete_skill"),
+    path("add-skill/<int:skill_id>/", views.addUserSkill, name="add_skill"),
 ]
